66278596e486204e1ff7c3f22088acf8961f94c2
RajnishKant <87873597+RajnishKant@users.noreply.github.com>
2021-08-26 13:19:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+	public static void main(String args[])
+	{
+		char c='a'; 
+		c=67;
+		System.out.println(c);
+		
+		double d1=5;
+		int k=(int)5.6;
+		System.out.println(k);
+		
+		
+	}
+}
---
+++ MethodOverloadingDemo.java
@@ -0,0 +1,15 @@
+
+class Casio
+{
+	public void add(int i,int j)
+	{
+		System.out.println(i+j);
+	}
+}
+public class MethodOverloadingDemo {
+	public static void main(String[] args)
+	{
+		Casio obj = new Casio();
+		obj.add(5, 2);
+	}
+}
